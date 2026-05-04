322b2bb74193726a3d31d0f28ee3ac259fbe5c93
_111y <huy264128@gmail.com>
2026-05-04 13:34:54 +0800

    merge

--- platform/shizuku_service/src/main/java/com/projectmaidgroup/platform/shizuku_service/shizuku_service.kt
+++ platform/shizuku_service/src/main/java/com/projectmaidgroup/platform/shizuku_service/shizuku_service.kt
@@ -1,8 +1,7 @@
 package com.projectmaidgroup.platform.shizuku_service
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+
 import kotlin.system.exitProcess
-import projectmaidgroup.maidlang.*
+
 class ShizukuServiceImpl : IShizukuService.Stub() {
     override fun destroy() {
         exitProcess(0)
@@ -17,34 +16,4 @@ class ShizukuServiceImpl : IShizukuService.Stub() {
         }
     }
 
-
-    override fun execMaidLang(code:String): String{
-        val interpreter=Interpreter()
-        //loat Tap
-        interpreter.registerNative("tap") {
-            InputHelper.tap(it[0].asFloat(), it[1].asFloat())
-            MaidValue.NullVal
-        }
-        interpreter.registerNative("swipe"){
-            InputHelper.swipe(it[0].asFloat(),it[1].asFloat(),it[2].asFloat(),it[3].asFloat(),it[4].asInt().toLong())
-            MaidValue.NullVal
-        }
-        interpreter.registerNative("sleep"){
-            delay(it[0].asInt().toLong())
-            MaidValue.NullVal
-        }
-        val inbuiltCode="external fun tap(float,float) -> void;" +
-                "external fun swipe(float,float,float,float,int);" +
-                "external fun sleep(int);"
-        val execCode=inbuiltCode+code
-        val program= runBlocking{ parser(lexer(execCode)).program() }
-        var result=""
-        for (node in program.codes) {
-            val currentResult = runBlocking{ interpreter.interpret(node) }
-            if (currentResult !is MaidValue.NullVal) {
-                result+=currentResult
-            }
-        }
-        return result
-    }
 }
